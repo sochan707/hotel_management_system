6ec4fa7a2e1f0a0cb5260c378e5ec81f0b49fddf
sochan707 <sochan7621@gmail.com>
2026-01-26 11:26:17 +0700

    sth

---
+++ demo/src/main/java/hotel/javabeans/App.java
@@ -0,0 +1,13 @@
+package hotel.javabeans;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
